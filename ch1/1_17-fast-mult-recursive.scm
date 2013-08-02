c619cd8c697e0f2e02eff23a9620c380d04bba47
fau <fau999@gmail.com>
2013-08-02 16:11:39 +0200

    Added Ex. 1.19, fixed swapped files

--- ch1/1_17-fast-mult-recursive.scm
+++ ch1/1_17-fast-mult-recursive.scm
@@ -8,18 +8,11 @@
 (define (even? x)
   (= (remainder x 2) 0))
 
-(define (fast-mult-iter a b c)
-  (cond ((= b 0) 0) 
-        ((= b 1) (+ c a))
-        ((even? b) (fast-mult-iter (double a)
-                                   (halve b)
-                                   c))
-        (else (fast-mult-iter a
-                              (- b 1)
-                              (+ c a)))))
-
 (define (fast-mult a b)
-  (fast-mult-iter a b 0))
+  (cond ((= b 0) 0) 
+        ((= b 1) a)
+        ((even? b) (fast-mult (double a) (halve b)))
+        (else (+ a (fast-mult a (- b 1))))))
 
 (display (fast-mult 11 12))
 (newline)
--- ch1/1_18-fast-mult-iter.scm
+++ ch1/1_18-fast-mult-iter.scm
@@ -8,11 +8,18 @@
 (define (even? x)
   (= (remainder x 2) 0))
 
-(define (fast-mult a b)
+(define (fast-mult-iter a b c)
   (cond ((= b 0) 0) 
-        ((= b 1) a)
-        ((even? b) (fast-mult (double a) (halve b)))
-        (else (+ a (fast-mult a (- b 1))))))
+        ((= b 1) (+ c a))
+        ((even? b) (fast-mult-iter (double a)
+                                   (halve b)
+                                   c))
+        (else (fast-mult-iter a
+                              (- b 1)
+                              (+ c a)))))
+
+(define (fast-mult a b)
+  (fast-mult-iter a b 0))
 
 (display (fast-mult 11 12))
 (newline)
